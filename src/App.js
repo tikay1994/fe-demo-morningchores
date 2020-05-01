4322558fe466ada8dae10d63d5fc3e530b9003c5
Tikay <kiet211194@gmail.com>
2020-05-01 21:08:20 +0700

    add btn-menu

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import Page1 from "./components/Page1/page1";
 import Page2 from "./components/Page2/page2";
 import Page3 from "./components/Page3/page3";
 import Footer from "./components/footer";
+import ButtonMenu from "./components/buttonMenu";
 function App() {
   return (
     <div className="App">
-      <Menu />
-      <Page1 />
-      <Page2 />
-      <Page3 />
-      <Footer />
+      {/* <Menu /> */}
+      {/* <Page1 /> */}
+      {/* <Page2 /> */}
+      {/* <Page3 /> */}
+      {/* <Footer /> */}
+      <ButtonMenu />
     </div>
   );
 }
